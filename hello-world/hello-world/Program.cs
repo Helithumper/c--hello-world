c4c7d512d2bd21d647c9ef3b3e90445f0b644121
Peyton Duncan <peyton.duncan45@gmail.com>
2016-11-27 23:50:23 -0500

    Initial Import

---
+++ hello-world/hello-world/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace helloworld
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			int[] list = new int[1000];
+
+			for (int i = 0; i < list.Length; i++) {
+				list [i] = i;
+			}
+
+			Console.WriteLine ("DONE writing");
+
+			foreach (int i in list) {
+				Console.WriteLine (i);
+			}
+			Console.WriteLine ("TYPE SOMETHING!!");
+			String userinput = Console.In.ReadLine ();
+			Console.WriteLine (userinput);
+
+		}
+	}
+}
